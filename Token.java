2ee14893868054baae2ade08b02c94d22a9781f3
w2444866290 <63185313+w2444866290@users.noreply.github.com>
2020-10-11 09:50:52 +0000

    Update Token.java

--- Token.java
+++ Token.java
@@ -40,25 +40,25 @@ public class Token {
                         String reserve = run.token.toString();
                         switch (reserve) {
                             case "BEGIN":
-                                System.out.print("Begin/r/n");
+                                System.out.print("Begin\r\n");
                                 break;
                             case "END":
-                                System.out.print("End/r/n");
+                                System.out.print("End\r\n");
                                 break;
                             case "FOR":
-                                System.out.print("For/r/n");
+                                System.out.print("For\r\n");
                                 break;
                             case "IF":
-                                System.out.print("If/r/n");
+                                System.out.print("If\r\n");
                                 break;
                             case "THEN":
-                                System.out.print("Then/r/n");
+                                System.out.print("Then\r\n");
                                 break;
                             case "ELSE":
-                                System.out.print("Else/r/n");
+                                System.out.print("Else\r\n");
                                 break;
                             default:
-                                System.out.print("Ident(" + reserve + ")/r/n");
+                                System.out.print("Ident(" + reserve + ")\r\n");
                         }
                         run.token = new StringBuilder();
                         continue;
@@ -72,36 +72,36 @@ public class Token {
                             run.token.append((char)n);
                             n = input.read();
                         }
-                        System.out.print("Int(" + run.token.toString() + ")/r/n");
+                        System.out.print("Int(" + run.token.toString() + ")\r\n");
                         run.token = new StringBuilder();
                         continue;
                     }
                     else if ((char)n == '+') {
-                        System.out.print("Plus/r/n");
+                        System.out.print("Plus\r\n");
                     }
                     else if ((char)n == '*') {
-                        System.out.print("Star/r/n");
+                        System.out.print("Star\r\n");
                     }
                     else if ((char)n == ',') {
-                        System.out.print("Comma/r/n");
+                        System.out.print("Comma\rn");
                     }
                     else if ((char)n == '(') {
-                        System.out.print("LParenthesis/r/n");
+                        System.out.print("LParenthesis\r\n");
                     }
                     else if ((char)n == ')') {
-                        System.out.print("RParenthesis/r/n");
+                        System.out.print("RParenthesis\r\n");
                     }
                     else if ((char)n == ':') {
                         n = input.read();
                         if ((char)n == '=')
-                            System.out.print("Assign/r/n");
+                            System.out.print("Assign\r\n");
                         else {
-                            System.out.print("Colon/r/n");
+                            System.out.print("Colon\r\n");
                             continue;
                         }
                     }
                     else {
-                        System.out.print("Unknown/r/n");
+                        System.out.print("Unknown\r\n");
                         break;
                     }
                     n = input.read();
